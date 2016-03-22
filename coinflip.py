320046320db4ca79da3ded8c28540f5fd979d8d2
ElDroidge <manan995@gmail.com>
2016-03-22 18:17:22 +0530

    Python Coinflip
    Removed Readme from within the script.

--- coinflip.py
+++ coinflip.py
@@ -1,10 +1,6 @@
-#PYCOINFLIP is a simple coinflip generator that runs on Python2.
-#It calculates whether the imaginary coin lands heads up or tails up
-#based on the "random" Python module. The "random" module allows the
-#program to pick up any floating-point number between 0 and 1. If the
-#result of the "random" computation is less than 0.5, the result is heads.
-#Otherwise, the result is tails.
+#PYCOINFLIP
 
+#Initialisation
 import random
 def lineBreak(): print("----------")
 
